d64ae04e52efd1d1ca2729ca1165459f835eab4d
Preston <prestonosoro56@gmail.com>
2023-02-13 11:00:33 +0300

    feat:add attribute accessor

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,18 @@
 ## code your solution here. 
+class Cat
+    #define micro method
+    attr_accessor :name
+    #meow method
+    def meow
+        puts "meow!"
+    end
+end
+
+#instance method
+spiky = Cat.new
+#Call cat name
+spiky.name = "Spiky"
+puts spiky.name
+#Call meow method
+spiky.meow
+
